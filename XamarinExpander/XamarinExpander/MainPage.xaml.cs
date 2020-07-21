7ca24c23d5b316fa61b9cd9d2cf93ee50ccedf62
Doniyor Niyozov <doniyozov@eaglebusinesssoftware.com>
2020-07-20 20:39:59 -0400

    Add project files.

---
+++ XamarinExpander/XamarinExpander/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinExpander
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = this;
+            InitializeComponent();
+
+            Messages = new ObservableCollection<Message>();
+            Messages.Add(new Message() { Header = "Close the main window.", Detail = "To be able to see this content you need to close the main window", Solution = "Press X button on the right corner or use shortcut Ctrl + SHift + C" });
+            Messages.Add(new Message() { Header = "Close the main window.", Detail = "To be able to see this content you need to close the main window", Solution = "Press X button on the right corner or use shortcut Ctrl + SHift + C" });
+            Messages.Add(new Message() { Header = "Close the main window.", Detail = "To be able to see this content you need to close the main window", Solution = "Press X button on the right corner or use shortcut Ctrl + SHift + C" });
+            Messages.Add(new Message() { Header = "Close the main window.", Detail = "To be able to see this content you need to close the main window", Solution = "Press X button on the right corner or use shortcut Ctrl + SHift + C" });
+            Messages.Add(new Message() { Header = "Close the main window.", Detail = "To be able to see this content you need to close the main window", Solution = "Press X button on the right corner or use shortcut Ctrl + SHift + C" });
+
+     } 
+
+        private ObservableCollection<Message> messages;
+
+        public ObservableCollection<Message> Messages
+        {
+            get { return messages; }
+            set { messages = value; OnPropertyChanged(); }
+        }
+
+        //#region Notify
+
+        ///// <summary>
+        ///// Property Changed event
+        ///// </summary>
+        //event PropertyChangedEventHandler PropertyChanged;
+
+        ///// <summary>
+        ///// Fire the PropertyChanged event
+        ///// </summary>
+        ///// <param name="propertyName">Name of the property that changed (defaults from CallerMemberName)</param>
+        //void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //#endregion
+
+
+    }
+}
---
+++ XamarinExpander/XamarinExpander/Message.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XamarinExpander
+{
+    public class Message : INotifyPropertyChanged
+    {
+        string header;
+        public string Header
+        {
+            get => header;
+            set { header = value; OnPropertyChanged(); }
+        }
+
+        string detail;
+        public string Detail
+        {
+            get => detail;
+            set { detail = value; OnPropertyChanged(); }
+        }
+
+        string solution;
+        public string Solution
+        {
+            get => solution;
+            set { solution = value; OnPropertyChanged(); }
+        }
+
+
+
+        #region Notify
+
+        /// <summary>
+        /// Property Changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Fire the PropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed (defaults from CallerMemberName)</param>
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //PLEASE NOTE: if possible, inherit from Eagle.Core's NotifyBase instead of manually implementing it! //TODO: Implement INotifyPropertyChanged
+
+        #endregion
+    }
+}
